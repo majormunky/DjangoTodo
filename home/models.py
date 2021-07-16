31acf5d7bedf033db73a353331d09f50c7588244
Josh Bright <josh.bright@gmail.com>
2021-07-15 17:45:18 -0700

    Added a TodoList model

--- home/models.py
+++ home/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+class TodoList(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
 class Todo(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.CharField(max_length=512)
