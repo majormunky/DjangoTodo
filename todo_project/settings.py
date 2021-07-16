21311854d14d6a6d27621b03df402c9dbb5a8c02
Josh Bright <josh.bright@gmail.com>
2021-07-15 21:42:33 -0700

    Updated settings to pull the debug value from .env

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
