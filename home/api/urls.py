013679c658ff93ddf733d41c0f5929eaba793ea1
Josh Bright <josh.bright@gmail.com>
2021-07-15 19:17:22 -0700

    We can now create new todo lists

--- home/api/urls.py
+++ home/api/urls.py
@@ -3,4 +3,5 @@ from home.api import views
 
 urlpatterns = [
     path("todo-list/", views.TodoListAPIView.as_view()),
+    path("todo-list/create/", views.TodoListCreateAPIView.as_view()),
 ]
--- home/api/views.py
+++ home/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import views
+from rest_framework import generics
 from rest_framework.response import Response
 from home.api import serializers
 from home import models
@@ -12,3 +13,13 @@ class TodoListAPIView(views.APIView):
         todo_lists = models.TodoList.objects.filter(user=request.user)
         json_lists = serializers.TodoListSerializer(todo_lists, many=True).data
         return Response(json_lists)
+
+
+class TodoListCreateAPIView(generics.CreateAPIView):
+    def post(self, request):
+        print(request.POST)
+        name = request.POST.get("name")
+        new_todo_list = models.TodoList(name=name, user=request.user)
+        new_todo_list.save()
+        json_data = serializers.TodoListSerializer(new_todo_list).data
+        return Response(json_data)
